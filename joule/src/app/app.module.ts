fc465af586dfc48b3963f7776bcd923d09d2b2ad
Andy <andy.rash1996@gmail.com>
2018-04-09 21:15:51 -0500

    added some custom styling; added front-page, login, and registration components (nothing there yet)

--- joule/src/app/app.module.ts
+++ joule/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		LoginComponent,
+		RegistrationComponent,
+		FrontPageComponent
 	],
 	imports: [
 		BrowserModule
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
