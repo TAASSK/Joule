c6a59774310b80acd7e8c3b4da6b7d92de040a53
samgiles <samdotgiles@gmail.com>
2018-04-24 20:56:58 -0500

    deleted accidental merge conflict additions in server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ server.connection({
 	host: '0.0.0.0',
 	port: 3000
 });
-var sha1 = require('sha1');
 
 // adds global URI path prefix to incoming requests
 // e.g. <domain>/api/dummy will get routed to /dummy
@@ -29,82 +28,6 @@ var connection = mysql.createConnection({
 
 //REVIEW ROUTES
 //adding a new review
-<<<<<<< HEAD
-server.route({
-        method: 'POST',
-        path: '/addReview',
-        handler: function(request, reply) {
-          var employee_num = request.payload.employee_num;
-          var hotness = request.payload.hotness;
-          var accountability = request.payload.accountability;
-          var availability = request.payload.availability;
-          var politeness = request.payload.politeness;
-          var efficiency = request.payload.efficiency;
-          var comments = request.payload.comments;
-          connection.query('INSERT INTO employee_review(employee_num, hotness, accountability, availability, politeness, efficiency, comments) VALUES("' + employee_num + '", "' + hotness + '", "' + accountability + '", "' + availability + '","' + politeness + '","' + efficiency + '", "' + comments + '")', function (error, results, fields) {
-           if (error)
-             throw error;
-          reply ('Review added to employee: ' + employee_num + '. Hotness: ' + hotness + '.');
-          console.log(results);
-        });
-      }
-});
-
-//USER ACCOUNT ROUTES
-//adding a new user -> making an account
-server.route({
-    method: 'POST',
-    path: '/newUser',
-    handler: function(request, reply) {
-        var username = request.payload.username;
-        var password = sha1(request.payload.password);
-        var first_name = request.payload.first_name;
-        var last_name = request.payload.last_name;
-        var employee_num = request.payload.employee_num;
-        var department_name = request.payload.department_name;
-        var position = request.payload.position;
-        var email = request.payload.email;
-        var employer = request.payload.employer;
-        var location = request.payload.location;
-        connection.query('INSERT INTO employee(username, password, first_name, last_name, employee_num, department_name, position, email, employer, location) VALUES("' + username + '", "' + password + '", "' + first_name + '", "' + last_name + '","' + employee_num + '","' + department_name + '", "' + position + '", "' + email + '", "' + employer +'", "' + location + '")', function (error, results, fields) {
-            if (error)
-                throw error;
-            reply('Employee Added: ' + first_name + ', '+ last_name);
-            console.log(results);
-        });
-    }
-});
-
-//User getting all of their reviews
-server.route({
-    method: 'GET',
-    path: '/getEmployeeRevs/{eid}',
-    handler: function (request, reply) {
-        console.log('Server processing a /getEmployeeRevs request');
-        const eid = request.params.eid;
-        connection.query('SELECT hotness, accountability, availability, politeness, efficiency, comments FROM employee_review WHERE employee_num="' + eid + '"', function (error, results, fields) {
-            if (error)
-                throw error;
-            reply(results);
-            console.log(results);
-        });
-
-    }
-});
-
-//SEARCH ROUTES
-//search by employee number
-server.route({
-    method: 'GET',
-    path: '/company/{name}',
-    handler: function (request, reply) {
-        console.log('Server processing a /company/{name} request');
-        const name = request.params.name;
-        connection.query('SELECT first_name,last_name FROM employee WHERE employer="' + name + '"', function (error, results, fields) {
-            if (error)
-                throw error;
-            reply (results);
-=======
 server.route({
         method: 'POST',
         path: '/addReview',
@@ -145,25 +68,11 @@ server.route({
             if (error)
                 throw error;
             reply('Employee Added: ' + first_name + ', '+ last_name);
->>>>>>> dev
             console.log(results);
         });
     }
 });
 
-<<<<<<< HEAD
-
-//Getting all employee info
-server.route({
-    method: 'GET',
-    path: '/getEmployees',
-    handler: function (request, reply) {
-        console.log('Server processing a /getEmployees request');
-        connection.query('SELECT * FROM employee', function (error, results, fields) {
-            if (error)
-                throw error;
-            reply (results);
-=======
 //User getting all of their reviews
 server.route({
     method: 'GET',
@@ -175,43 +84,12 @@ server.route({
             if (error)
                 throw error;
             reply(results);
->>>>>>> dev
             console.log(results);
         });
 
     }
 });
 
-<<<<<<< HEAD
-//Deleting a user and all of their reviews
-server.route({
-    method: 'DELETE',
-    path: '/deleteUser/{eid}',
-    handler: function (request, reply) {
-        console.log('Server processing a /deleteUser request');
-        const eid = request.params.eid;
-        connection.query('DELETE FROM employee WHERE employee_num="' + eid + '"' , function (error, results, fields) {
-            if (error)
-                throw error;
-            reply ('Account deleted for employee with id: ' + eid);
-            console.log(results);
-        });
-        connection.query('DELETE FROM employee_review WHERE employee_num="' + eid + '"' , function (error, results, fields) {
-            if (error)
-                throw error;
-            console.log(results);
-        });
-    }
-});
-
-//Getting all employee info
-server.route({
-    method: 'GET',
-    path: '/getReviews',
-    handler: function (request, reply) {
-        console.log('Server processing a /getReviews request');
-        connection.query('SELECT * FROM employee_review', function (error, results, fields) {
-=======
 //SEARCH ROUTES
 //search by employee number
 server.route({
@@ -237,7 +115,6 @@ server.route({
     handler: function (request, reply) {
         console.log('Server processing a /getEmployees request');
         connection.query('SELECT * FROM employee', function (error, results, fields) {
->>>>>>> dev
             if (error)
                 throw error;
             reply (results);
@@ -247,24 +124,6 @@ server.route({
     }
 });
 
-<<<<<<< HEAD
-server.route({
-	method: 'PUT',
-		path: '/updateUser',
-			handler: function (request, reply) {
-					console.log('Server is updating a user profile...');
-					var first_name = request.payload.first_name;
-					var last_name = request.payload.last_name;
-					var email = request.payload.email;
-					var company = request.payload.company;
-					var password = request.payload.password;
-					var current_emp_no = request.payload.current_emp_no;
-					connection.query('UPDATE employee SET first_name = "' + first_name + '", last_name = "' + last_name + '", email = "' + email + '", employer = "' + company + '", password = "' + password + '" WHERE employee_num = "' + current_emp_no + '";', function (error, results, fields) {
-						reply('Information updated for employee number: ' + current_emp_no);
-					});
-			}
-		});
-=======
 //Deleting a user and all of their reviews
 server.route({
     method: 'DELETE',
@@ -301,7 +160,6 @@ server.route({
 
     }
 });
->>>>>>> dev
 
 server.start((err) => {
 
@@ -309,8 +167,4 @@ server.start((err) => {
         throw err;
     }
     console.log(`Server running at: ${server.info.uri}`);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> dev
