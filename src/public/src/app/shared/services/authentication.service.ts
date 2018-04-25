be69994dd0ca142ef591400927d6836417688eb8
Tristan Knotts <tristanknotts@gmail.com>
2018-04-24 21:43:56 -0500

    login authentication service function

--- src/public/src/app/shared/services/authentication.service.ts
+++ src/public/src/app/shared/services/authentication.service.ts
@@ -2,15 +2,21 @@
  * Angular library
  * */
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from './../../shared';
 
 @Injectable()
 export class AuthenticationService {
 
+constructor(protected httpClient: HttpClient) {
+}
 	isAuthenticated(): boolean {
 		return true;
 	}
 
-	logIn() {}
+	logIn(email: string, password: string) {
+    return this.httpClient.post<User>('/localhost:8080/api/login', {email, password});
+}
 
 	logOut() {}
 
