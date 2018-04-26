f6dc102f9774fbfa4fd0069b97a4eee523cbaf7f
Tristan Knotts <tristanknotts@gmail.com>
2018-04-25 20:44:40 -0500

    authentication updates

--- src/public/src/app/shared/services/authentication.service.ts
+++ src/public/src/app/shared/services/authentication.service.ts
@@ -4,6 +4,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from './../../shared';
+import  'rxjs/add/operator/do';
+import * as moment from './../../../../node_modules/moment';
 
 @Injectable()
 export class AuthenticationService {
@@ -15,9 +17,21 @@ constructor(protected httpClient: HttpClient) {
 	}
 
 	logIn(email: string, password: string) {
-    return this.httpClient.post<User>('/localhost:8080/api/login', {email, password});
+    return this.httpClient.post<User>('/localhost:8080/api/login', {email, password})
+    .do(res => this.setSession);
 }
 
-	logOut() {}
 
+
+  private setSession(authResult) {
+    const expiresAt = moment().add(authResult.expiresIn, 'second');
+
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()) );
+}
+
+logOut() {
+  localStorage.removeItem('id_token');
+  localStorage.removeItem('expires_at');
+}
 }
