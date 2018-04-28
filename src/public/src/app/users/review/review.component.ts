93a20f8b7466d64fa55dfb9c494987fb8df26cf7
Andy <andy.rash1996@gmail.com>
2018-04-28 17:49:05 -0500

    initial styling for reviews

--- src/public/src/app/users/review/review.component.ts
+++ src/public/src/app/users/review/review.component.ts
@@ -7,14 +7,12 @@ import {
 	NgModule,
 	OnInit
 } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 /*
  * Models
  * */
-import {
-	Review,
-	User
-} from '../../shared';
+import { Review } from '../../shared';
 
 @Component({
 	selector: 'app-review',
@@ -22,59 +20,21 @@ import {
 })
 export class ReviewComponent implements OnInit {
 
-	user: User;
 	review: Review;
 
 	constructor() {
 
-		this.user = new User();
 		this.review = new Review();
 
-		this.user.email = "";
-		this.user.email = "";
-		this.user.firstName = "";
-		this.user.lastName = "";
-		this.user.password = "";
-		this.user.jobTitle = "";
-		this.user.employer = "";
-		this.user.location = "";
-
-		this.review.jobTitle = "";
-		this.review.employer = "";
+		this.review.jobTitle = 'Employee';
+		this.review.employer = 'Random Corp.';
 		this.review.qualityRating = 78;
 		this.review.recommendRating = 84;
-		this.review.comment = "";
+		this.review.comment = 'They showed up to work.';
 		this.review.datestamp = new Date();
-
-		var userObj = {
-			email: this.email,
-			first_name: this.firstName,
-			last_name: this.lastName,
-			password: this.password,
-			job_title: this.jobTitle,
-			employer: this.employer,
-			location: this.location
-		};
-
-		var reviewObj = {
-			job_title: this.jobTitle,
-			employer: this.employer,
-			quality_rating: this.qualityRating,
-			recommend_rating: this.recommendRating,
-			comment: this.comment,
-			datestamp: this.datestamp
-		};
 
 	}
 
 	ngOnInit() { }
-
-	logUserSerialize() {
-		console.log(this.user.serialize());
-	}
-
-	logReviewSerialize() {
-		console.log(this.review.serialize());
-	}
 
 }
