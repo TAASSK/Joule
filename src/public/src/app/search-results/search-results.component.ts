af9e0233ec675e173462e783e763105170bb8af5
Andy <andy.rash1996@gmail.com>
2018-04-30 18:07:26 -0500

    dummy search results + properly spaced result containers

--- src/public/src/app/search-results/search-results.component.ts
+++ src/public/src/app/search-results/search-results.component.ts
@@ -34,6 +34,36 @@ export class SearchResultsComponent implements OnInit {
 			this.query = params['q'];
 		});
 
+		var user1 = new User();
+		var user2 = new User();
+		var user3 = new User();
+
+		user1.id = 101;
+		user1.email = 'jp.joule18@gojoule.me';
+		user1.firstName = 'John';
+		user1.lastName = 'Doe';
+		user1.jobTitle = 'Employee';
+		user1.employer = 'Random Corp.';
+		user1.location = 'Dallas, TX';
+
+		user2.id = 101;
+		user2.email = 'jp.joule18@gojoule.me';
+		user2.firstName = 'John';
+		user2.lastName = 'Doe';
+		user2.jobTitle = 'Employee';
+		user2.employer = 'Random Corp.';
+		user2.location = 'Dallas, TX';
+
+		user3.id = 101;
+		user3.email = 'jp.joule18@gojoule.me';
+		user3.firstName = 'John';
+		user3.lastName = 'Doe';
+		user3.jobTitle = 'Employee';
+		user3.employer = 'Random Corp.';
+		user3.location = 'Dallas, TX';
+
+		this.results.push(user1, user2, user3);
+
 	}
 
 	ngOnInit() { }
